0efffa4c21da17006e8bcfeedad22812f3c605ca
Reloaded <reloadtmm@gmail.com>
2018-01-26 15:38:19 +0100

    Hola Mundo en Java

---
+++ src/prueba/jenkins/Main.java
@@ -0,0 +1,20 @@
+package prueba.jenkins;
+
+public class Main {
+	
+	/**
+	 * @author Tasio Mateos	
+	 * @param args
+	 */
+
+	public Main() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
